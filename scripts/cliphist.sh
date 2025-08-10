a4f961de9245d655f14956c6f62ba4a9c8e8f646
Eli <elijah2307.williams@gmail.com>
2025-08-10 20:10:04 +0100

    wofi + revamp of waybar

--- scripts/cliphist.sh
+++ scripts/cliphist.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-#   ____ _ _       _     _     _    
-#  / ___| (_)_ __ | |__ (_)___| |_  
-# | |   | | | '_ \| '_ \| / __| __| 
-# | |___| | | |_) | | | | \__ \ |_  
-#  \____|_|_| .__/|_| |_|_|___/\__| 
-#           |_|                     
-#  
-# by Stephan Raabe (2023) 
-# ----------------------------------------------------- 
 
-case $1 in
-    d) cliphist list | rofi -dmenu -replace -config ~/dotfiles/rofi/config-cliphist.rasi | cliphist delete
-       ;;
 
-    w) if [ `echo -e "Clear\nCancel" | rofi -dmenu -config ~/dotfiles/rofi/config-short.rasi` == "Clear" ] ; then
-            cliphist wipe
-       fi
-       ;;
+cliphist list | wofi -d -c ~/dotfiles/wofi/cliphistconfig -s ~/dotfiles/wofi/style.css| cliphist decode | wl-copy 
 
-    *) cliphist list | rofi -dmenu -replace -config ~/dotfiles/rofi/config-cliphist.rasi | cliphist decode | wl-copy
-       ;;
-esac
