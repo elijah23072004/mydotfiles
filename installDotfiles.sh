3a2b211d5f86d6d6ca5213252a4313a570e68500
Eli <elijah2307.williams@gmail.com>
2025-08-14 18:00:50 +0100

    fixed install script

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -1,22 +1,36 @@
 
-packages="glib2 base-devel alactritty eww hypr kitty neovim starship vim firefox thunar hyprland hypridle hyprpaper cliphist swww hyprcursor hyprlock hyprshot hyprsunset spotifty-launcher pactl blueman bluez pulseaudio pulseaudio-bluetooth brightnessctl playerctl python nmtui networkmanager pavucontrol git wget"
+packages="glib2 base-devel alacritty kitty neovim starship vim firefox thunar hyprland hypridle hyprpaper ly cliphist swww hyprcursor hyprlock hyprshot hyprsunset spotify-launcher blueman bluez pulseaudio pulseaudio-bluetooth brightnessctl playerctl python networkmanager pavucontrol git wget"
 sudo pacman -Syu
 sudo pacman -S --needed $packages
 
 
 
-ln -S ~/dotfiles/alactritty  ~/.config/alactritty 
-ln -S ~/dotfiles/eww ~/.config/eww
-ls -S ~/dotfiles/hypr ~/.config/hypr
-ln -S ~/dotfiles/.bashrc ~/.bashrc
-ln -S ~/dotfiles/kitty ~/.config/kitty
-ln -S ~/dotfiles/nvim ~/.config/nvim
-ln -S ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -S ~/dotfiles/swaync ~/.config/swaync 
-ln -S ~/dotfiles/vim ~/.config/vim
-ln -S ~/dotfiles/waybar ~/.config/waybar
-ln -S ~/dotfiles/wlogout ~/.config/wlogout
-ln -S ~/dotfiles/wofi ~/.config/wofi 
+#setup yay and fonts 
+sudo rm -r yay 
+git clone https://aur.archlinux.org/yay.git
+cd yay 
+makepkg -si 
+sudo rm -r yay
+
+
+fonts = "ttf-nerd-fonts-symmbols  awaita-fonts fontconig otf-font-awesome terminus-font woff2-font-awesome xorg-fonts-util ttf-roboto-mono ttf-fireacode-nerd"
+yay -S --needed eww fonts
+
+
+
+
+ln -S ./alactritty  ~/.config/alactritty 
+ln -S ./eww ~/.config/eww
+ls -S .//hypr ~/.config/hypr
+ln -S ./.bashrc ~/.bashrc
+ln -S ./kitty ~/.config/kitty
+ln -S ./nvim ~/.config/nvim
+ln -S ./starship/starship.toml ~/.config/starship.toml
+ln -S ./swaync ~/.config/swaync 
+ln -S ./vim ~/.config/vim
+ln -S ./waybar ~/.config/waybar
+ln -S ./wlogout ~/.config/wlogout
+ln -S ./wofi ~/.config/wofi 
 
 
 
@@ -27,22 +41,5 @@ sudo systemctl enable ly.service
 
 
 #add gtk theme 
-ln -S ~/dotfiles/gtk .themes
+ln -S ./gtk .themes
 gsettings set org.gnome.desktop.interface gtk-theme diinki-retro-dark
-
-
-
-#setup yay and fonts 
-mkdir ~/.cache/installer 
-cd ~/.cache/installer 
-git clone https://aur.archlinux.org/yay.git
-cd yay 
-makepkg -si 
-cd ../.. 
-rm -r installer 
-
-
-fonts = "ttf-nerd-fonts-symmbols  awaita-fonts fontconig otf-font-awesome terminus-font woff2-font-awesome xorg-fonts-util ttf-roboto-mono ttf-fireacode-nerd"
-
-yay -S fonts
-
