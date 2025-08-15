c9050db1afcee7f267b648b1813681e109079c1a
Eli <elijah2307.williams@gmail.com>
2025-08-15 19:05:41 +0100

    added hyprland to nix config

--- configuration.nix
+++ configuration.nix
@@ -168,7 +168,30 @@ fonts.packages = with pkgs; [
   nerd-fonts.noto
 
 ];
+{
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
+  programs.hyprlock.enable = true;
+  services.hypridle.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    pyprland
+    hyprpicker
+    hyprcursor
+    hyprlock
+    hypridle
+    hyprpaper
+    hyprpolkitagent
+
+
+  ];
+}
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
