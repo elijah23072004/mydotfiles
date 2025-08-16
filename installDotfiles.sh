8b89de652b7b3776aae56611de3c94031bd55072
Eli <elijah2307.williams@gmail.com>
2025-08-16 16:43:47 +0100

    made installer more intuiative to use

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -1,18 +1,17 @@
-
-if [ ! "$#" -eq 2 ] ; then 
-    echo "please pass absolute path of dotfiles as argument 1 and for argument 2 the operating system. 0 for arch linux and 1 for nixos"
-    exit
-fi
-echo $1
-echo $2
-
 dotfilesLocation=$1
+if [ ! "$#" -eq 1 ] ; then 
+    read -p "please pass absolute path of dotfiles:- " dotfilesLocation 
+fi
+if [ ! -d "$dotfilesLocation" ]; then
+    echo "Invalid path" 
+    exit
+fi 
+echo $dotfilesLocation
 
 #ask for which os to use 0 : arch,  1 : nix  
-os=$2
-if [ $os -eq 0 ] ; then 
+read -p "Which linux distribution is running: 0 - arch, 1 - nixos:-" choice 
+if [ "$choice" = "0" ]; then 
     echo "arch" 
-
     packages="python-pywal glib2 base-devel alacritty kitty neovim starship vim firefox thunar hyprland hypridle hyprpaper ly cliphist swww hyprcursor hyprlock hyprshot hyprsunset spotify-launcher blueman bluez pulseaudio pulseaudio-bluetooth brightnessctl playerctl python networkmanager pavucontrol git wget"
 
     sudo pacman -Syu                                                                
@@ -39,7 +38,7 @@ if [ $os -eq 0 ] ; then
      sudo cp $dotfilesLocation/ly/config.ini /etc/ly/config.ini
 
 
- elif [ $os -eq 1 ] ; then 
+ elif [ "$choice" = "1" ] ; then 
      echo "nix"
      sudo cp  $dotfilesLocation/configuration.nix /etc/nixos/configuration.nix
 
@@ -50,7 +49,7 @@ if [ $os -eq 0 ] ; then
      echo "invalid os" 
      exit
 fi 
-./createSymLinks.sh $dotfilesLocation
+./scripts/createSymLinks.sh $dotfilesLocation
 
 
 cp $dotfilesLocation/wal ~/.cache/wal -r 
