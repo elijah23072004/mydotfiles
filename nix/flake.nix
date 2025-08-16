1be007976f10825f13d25b330db66e0b88a35b23
eli <elijah2307.williams@gmail.com>
2025-08-16 21:27:41 +0100

    initial nixos setup

---
+++ nix/flake.nix
@@ -0,0 +1,33 @@
+{
+    description="My system config";
+
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+        home-manager = {
+            url = "github:nix-community/home-manger";
+            inputs.nixpkgs.follows="nixpkgs";
+        };
+
+    };
+
+    outputs = {self, nixpkgs, home-manager, ...}@inputs:
+        let 
+            system = "x86_64-linux";
+        in {
+
+            nixosConfigurations.elinix = nixpkgs.lib.nixosSystem {
+                inherit inputs system;
+            };
+            modules = [
+                ./nixos/configuration.nix
+            ];
+
+        };
+    homeConfigurations.eli = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.$(system);
+        modules = [ ./home-manager/home.nix];
+    };
+};
+
